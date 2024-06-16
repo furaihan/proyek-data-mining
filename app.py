521e50f20be1a2e596638dd3e319e8d1441bec43
Zhafar Al Fathi <28698437+furaihan@users.noreply.github.com>
2024-06-16 21:42:45 +0700

    feat(streamlit): add more logic to handle null input

--- app.py
+++ app.py
@@ -78,14 +78,23 @@ def process_input(text):
     return features + vectorized
 
 # Streamlit App
+st.set_page_config(page_title="Indonesian Clickbait Headline Detector", page_icon=":newspaper:", layout="centered")
 st.title('Indonesian Clickbait Headline Detector')
 user_input = st.text_input("Masukkan Judul Berita:")
 
-if st.button('Preprocess'):
+preprocess_button = st.button('Preprocess')
+if preprocess_button:
+    if not user_input:
+        st.write('Masukkan judul berita terlebih dahulu!')
+        st.stop()
     processed_data = process_input(user_input)
-    st.write(processed_data)
+    st.dataframe(processed_data)
 
-if st.button('Predict'):
+predict_button = st.button('Predict')
+if predict_button:
+    if not user_input:
+        st.write('Masukkan judul berita terlebih dahulu!')
+        st.stop()
     processed_data = process_input(user_input)
     prediction = model.predict([processed_data])[0]
     result = 'Headline ini bukan clickbait' if prediction == 0 else 'Headline ini clickbait'
